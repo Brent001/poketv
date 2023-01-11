b2be93494c04cb21bfe953426492b29a41561b0d
Lamine <pridila.2006@gmail.com>
2023-01-11 01:24:41 +0100

    navbar design

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,10 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { FaFacebook, FaInstagram, FaTwitter } from "react-icons/fa";
+import { MdCatchingPokemon } from "react-icons/md";
 
 const Navbar = () => {
+  const socialLinks = [<FaFacebook />, <FaInstagram />, <FaTwitter />];
   const nav = [
     {
       title: "Home",
@@ -17,25 +20,40 @@ const Navbar = () => {
     },
   ];
   return (
-    <div className="h-[60] w-full p-4 flex justify-between">
-      <div>
-        <h2>PokeTV</h2>
-      </div>
-      <ul className="flex gap-2">
-        {nav.map((navLink) => (
-          <li>
-            <Link
-              to={navLink.link}
-              className="px-4 py-3 rounded-md text-sm font-medium hover:bg-gray-800 hover:text-white"
+    <div className="w-full h-14">
+      <div className="p-4 h-full max-w-[1200px] mx-auto flex justify-between items-center">
+        <div>
+          <h2 className="uppercase text-lg font-bold flex items-center gap-2">
+            <MdCatchingPokemon className="text-2xl text-red-500" />
+            <span>
+              Poke<span className="text-red-500">TV</span>
+            </span>
+          </h2>
+        </div>
+
+        <ul className="hidden md:flex gap-2">
+          {nav.map((navLink) => (
+            <li>
+              <Link
+                to={navLink.link}
+                className="px-4 py-3 rounded-md text-sm font-medium hover:bg-gray-800 hover:text-white"
+              >
+                {navLink.title}
+              </Link>
+            </li>
+          ))}
+        </ul>
+
+        <div className="hidden md:flex gap-2">
+          {socialLinks.map((link) => (
+            <a
+              href="https://"
+              className="rounded-full p-2 hover:bg-gray-800 hover:text-white text-xl"
             >
-              {navLink.title}
-            </Link>
-          </li>
-        ))}
-      </ul>
-      <div>
-        <a href="https://">
-        </a>
+              {link}
+            </a>
+          ))}
+        </div>
       </div>
     </div>
   );
