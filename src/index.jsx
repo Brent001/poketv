6aea8191221be02f938a4528d844f32ce65125f1
Lamine <pridila.2006@gmail.com>
2023-02-25 06:03:17 +0100

    google analytics

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,7 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import React from "react";
 import ReactDOM from "react-dom/client";
+import ReactGA from "react-ga4";
 import { Provider } from "react-redux";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./index.css";
@@ -36,8 +37,8 @@ const router = createBrowserRouter([
   },
 ]);
 
+ReactGA.initialize("G-0M5JPSSMNZ");
 const queryClient = new QueryClient();
-
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
