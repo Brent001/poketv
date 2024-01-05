cc9a17352987ff5ec8eba2ddc3ab9b0603db9e6a
Brent Abrazaldo <92303760+Brent001@users.noreply.github.com>
2024-01-05 20:35:52 +0800

    Update animes.js

--- src/api/animes.js
+++ src/api/animes.js
@@ -2,28 +2,28 @@ import axios from "axios";
 
 export const getAnimes = async () => {
   const res = await axios.get(
-    "https://api.consumet.org/meta/anilist/trending?perPage=60"
+    "https://kaido-api.vercel.app/meta/anilist/trending?perPage=60"
   );
   return res.data;
 };
 
 export const getAnimeDetails = async (id, dub) => {
   const res = await axios.get(
-    `https://api.consumet.org/meta/anilist/info/${id}?dub=${dub}`
+    `https://kaido-api.vercel.app/meta/anilist/info/${id}?dub=${dub}`
   );
   return res.data;
 };
 
 export const getStreamingLinks = async (id) => {
   const res = await axios.get(
-    `https://api.consumet.org/meta/anilist/watch/${id}`
+    `https://kaido-api.vercel.app/meta/anilist/watch/${id}`
   );
   return res.data;
 };
 
 export const findAnime = async (query, page) => {
   const res = await axios.get(
-    `https://api.consumet.org/meta/anilist/${query}?page=${page}`
+    `https://kaido-api.vercel.app/meta/anilist/${query}?page=${page}`
   );
   return res.data;
 };
